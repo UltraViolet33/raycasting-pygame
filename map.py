bd1f38ff2d3049a57cc01b144c90e603a7f0e450
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2022-12-30 21:02:36 +0100

    add game and map classes

---
+++ map.py
@@ -0,0 +1,26 @@
+import pygame
+from settings import *
+
+mini_map = (
+    '########'
+    '#  #   #'
+    '#  # ###'
+    '#      #'
+    '#      #'
+    '#  ##  #'
+    '#   #  #'
+    '########'
+)
+
+
+class Map:
+    def __init__(self, game):
+        self.game = game
+        self.mini_map = mini_map
+
+    def draw(self):
+        for row in range(8):
+            for col in range(8):
+                square = row * MAP_SIZE + col
+                pygame.draw.rect(self.game.screen, (200, 200, 200) if self.mini_map[square] == "#" else (
+                    100, 100, 100), (col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE-2, TILE_SIZE-2))
