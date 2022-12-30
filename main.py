6f25ff834397a9db190874177ad0a27ef57bff03
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2022-12-30 20:47:55 +0100

    add settings

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 import pygame
 import sys
 import math
-
+from settings import *
 
 # contants
-SCREEN_HEIGHT = 480
-SCREEN_WIDTH = SCREEN_HEIGHT * 2
 
-FOV = math.pi / 3
-HALF_FOV = FOV / 2
-
-MAP_SIZE = 8
-
-TILE_SIZE = int((SCREEN_WIDTH / 2) / MAP_SIZE)
-CASTED_RAYS = 120
-STEP_ANGLE = FOV / CASTED_RAYS
-
-MAX_DEPTH = int(MAP_SIZE * TILE_SIZE)
-
-
-SCALE = (SCREEN_WIDTH / 2) / CASTED_RAYS
 
 player_x = (SCREEN_WIDTH / 2) / 2
 player_y = (SCREEN_WIDTH / 2) / 2
